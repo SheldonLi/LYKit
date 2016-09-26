8a328680518bc80c89f32977876cf38c8d5591a6
Sheldon <lishaoyouqy@126.com>
2016-09-26 16:26:32 +0800

    project init

---
+++ LYKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYKit
+//
+//  Created by SheldonLee on 16/9/26.
+//  Copyright © 2016年 Sheldon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+pod 'AFNetworking', '~> 3.1.0'
+pod 'SDWebImage', '~> 3.7.3'
+pod 'SVProgressHUD', '~> 1.1.3'
+pod 'FMDB', '~> 2.5'
+
